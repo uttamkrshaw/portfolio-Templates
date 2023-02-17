d6fe1e29b15dcbd0f8d714c2646b75328d5d44f4
uttamkrshaw <uttamkr5599@gmail.com>
2023-02-17 16:36:06 +0530

    resume button done

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { AiOutlineDownload } from "react-icons/ai"
 
-function Resume() {
+const Resume = () => {
     return (
-        <div className="opacity-50 hover:opacity-100">
-            <a href="https://drive.google.com/file/d/1R0Gq3cFuy0t_2LlxlGQESS9mOV6Iehwn/view?usp=share_link" color="transparent"
-                target="_blank" download>
-                <button>Resume</button></a>
+        <div >
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1R0Gq3cFuy0t_2LlxlGQESS9mOV6Iehwn/view?usp=share_link" color="transparent"
+                target="_blank" download ><AiOutlineDownload /> Resume </a>
         </div>
     )
 }
