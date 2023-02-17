536a55ba21fb440b71bdf4c81dda51006b112d67
uttamkrshaw <uttamkr5599@gmail.com>
2023-02-17 17:02:44 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -141,7 +141,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='uttamkr5599@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,9 +3,9 @@ import { AiOutlineDownload } from "react-icons/ai"
 
 const Resume = () => {
     return (
-        <div >
+        <div className='hover:scale-110 hover:bg-sky-700  duration-500' >
             <a className='flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1R0Gq3cFuy0t_2LlxlGQESS9mOV6Iehwn/view?usp=share_link" color="transparent"
-                target="_blank" download ><AiOutlineDownload /> Resume </a>
+                target="_blank"  download={"https://drive.google.com/u/0/uc?id=1R0Gq3cFuy0t_2LlxlGQESS9mOV6Iehwn&export=download"} ><AiOutlineDownload /> Resume </a>
         </div>
     )
 }
